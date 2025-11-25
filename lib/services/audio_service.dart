dd1555bf20e55848b33c30e08b84eaf4f201e1b6
FrostSue <emirhan_58600@hotmail.com>
2025-11-25 19:28:29 +0300

    audio issues fixed

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -5,21 +5,19 @@ class AudioService {
   factory AudioService() => _instance;
   AudioService._internal();
 
-  
   final AudioPlayer _clickPlayer = AudioPlayer();
   final AudioPlayer _correctPlayer = AudioPlayer();
   final AudioPlayer _wrongPlayer = AudioPlayer();
-  
+  final AudioPlayer _owlPlayer = AudioPlayer();
   bool isMuted = false;
 
-  
   Future<void> init() async {
     await _clickPlayer.setPlayerMode(PlayerMode.lowLatency);
     await _correctPlayer.setPlayerMode(PlayerMode.lowLatency);
     await _wrongPlayer.setPlayerMode(PlayerMode.lowLatency);
+    await _owlPlayer.setPlayerMode(PlayerMode.lowLatency);
   }
 
-  
   Future<void> _play(AudioPlayer player, String path) async {
     if (isMuted) {
       await player.setVolume(0);
@@ -29,12 +27,9 @@ class AudioService {
     }
 
     try {
-      
       if (player.state == PlayerState.playing) {
         await player.stop();
       }
-      
-      
       await player.play(AssetSource(path));
     } catch (e) {
       print("Audio Error: $e");
@@ -44,4 +39,5 @@ class AudioService {
   void playCorrect() => _play(_correctPlayer, 'sounds/correct.wav');
   void playWrong() => _play(_wrongPlayer, 'sounds/wrong.wav');
   void playClick() => _play(_clickPlayer, 'sounds/click.mp3');
+  void playOwl() => _play(_owlPlayer, 'sounds/owl-click.mp3');
 }
